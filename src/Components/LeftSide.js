a95fef5fab0dd913b2095e60aff5fa2fc5510dd2
pacis32 <pacis32@gmail.com>
2020-09-04 11:06:49 -0500

    Discard

--- src/Components/LeftSide.js
+++ src/Components/LeftSide.js
@@ -1,15 +1,1 @@
-import React from 'react'
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-export default function LeftSide() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+
